3ec89dbea1b0b5067f3da37f3e6075781c5fbeb1
magicalwig34653 <linolaske@icloud.com>
2022-11-01 15:22:56 +0100

    bearbeitung des Projektes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Bitte geben Sie Ihr Alter an:");
+        int AlterDerPerson = scan.nextInt();
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("Bitte geben Sie Ihre Dauer der Betriebszugehörigkeit an:");
+        int GearbeiteteJahre = scan.nextInt();
+        System.out.println("---------------------------------------------------------------------");
+        boolean zugelassen = false;
+
+        if (AlterDerPerson >= 65 || GearbeiteteJahre >= 30) {
+            zugelassen = true;
+        } else if (AlterDerPerson >= 60 && GearbeiteteJahre >= 25) {
+            zugelassen = true;
+        }
+        if (zugelassen) {
+            System.out.println("=====================================================================");
+            System.out.println("Ihre Eingegebenen Daten:");
+            System.out.println("    Alter: " + AlterDerPerson);
+            System.out.println("    Betriebszugehörigkeit: " + GearbeiteteJahre);
+            System.out.println("=====================================================================");
+            System.out.println("=====================================================================");
+            System.out.println("Herzlichen Glückwunsch Sie dürfen pensioniert werden!");
+        } else {
+            System.out.println("=====================================================================");
+            System.out.println("Ihre Eingegebenen Daten:");
+            System.out.println("    Alter: " + AlterDerPerson);
+            System.out.println("    Betriebszugehörigkeit: " + GearbeiteteJahre);
+            System.out.println("=====================================================================");
+            System.out.println("=====================================================================");
+            System.out.println("Leider dürfen Sie noch nicht pensioniert werden!");
+        }
     }
 }
